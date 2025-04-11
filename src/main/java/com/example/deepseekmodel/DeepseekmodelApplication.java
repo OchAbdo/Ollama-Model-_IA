0a646713043d50eee6b8219e99538668437dfe79
OchAbdo <ochabdo02@gmail.com>
2025-04-11 22:59:47 +0100

    Ollama model local

---
+++ src/main/java/com/example/deepseekmodel/DeepseekmodelApplication.java
@@ -0,0 +1,13 @@
+package com.example.deepseekmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepseekmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepseekmodelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/deepseekmodel/DeepseekmodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deepseekmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepseekmodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
